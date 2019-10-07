71447eacb725a450f6ef681dedd9cf87ef4c440d
Robinson Santana Silverio <robinsntn@gmail.com>
2019-10-07 11:36:35 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,20 +1,13 @@
 # require modules here
-require "yaml"
 
-
-def load_library(get_meaning, get_emoticon)
-  file = File.read(get_meaning)
-  emoticons = YAML.load(file)
+def load_library
+  # code goes here
 end
 
-def get_japanese_emoticon(file_path, emoticon)
-  emoticons = load_library(file_path)
-  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+def get_japanese_emoticon
+  # code goes here
 end
 
 def get_english_meaning
-  emoticons = load_library(file_path, emoticon)
-  emoji = emoticons.find {|name, faces| faces[1] == emoticon }
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[0]
+  # code goes here
 end
